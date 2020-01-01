799e75061fcea063965fe07a817bf30ce79e697f
Javier Almarza <javieralmarzabucarey@gmail.com>
2020-01-01 16:36:51 -0400

    probando componentes

---
+++ components/message/network.js
@@ -0,0 +1,20 @@
+// Dependencies.
+const express = require("express");
+const responseController = require("../../network/response");
+const controller = require("./controller");
+
+// Create a Router
+const router = express.Router();
+
+// Routes manager
+router.get("/", (request, response) => {
+
+});
+
+router.post("/", (request, response) => {
+
+});
+
+router.options("/", (request, response) => {
+
+});
